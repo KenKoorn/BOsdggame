909c8c8b749ac537e279aa594b2f6ede8ab315fd
aymen <35670@ma-web.nl>
2023-11-01 18:16:26 +0100

    einde

--- sdgs.php
+++ sdgs.php
@@ -11,7 +11,7 @@ require_once( SOURCE_PATH . 'database.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Music-Info</title>
+    <title>info</title>
     <link rel="stylesheet" type="text/css" href="sdg.css">
 </head>
 <body>
