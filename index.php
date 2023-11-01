28f36a38e8f5e90cd555cb929512748425948719
aymen <35670@ma-web.nl>
2023-11-01 16:19:33 +0100

    phpaymen

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<<<<<<< HEAD:index.php
+<?php
+// haal al mijn instellingen op
+require_once('../source/config.php');
+
+// haal de database verbinding op
+require_once( SOURCE_PATH . 'database.php');
+?>
 <!DOCTYPE html>
 
 <head>
@@ -34,12 +40,26 @@
         <div class="container">
         <div id="results">
 
-<?php
-// Inclusief het singles.php-document om gegevens op te halen
-include '../source/data.php';
+        <?php
 
-foreach ($musicSingles as $single) {
-    include '../source/views/card.php';
+// handle incomming request
+// controleer de url, is er misschien een categorie geselecteerd?
+$url = explode('/', trim($_SERVER['REQUEST_URI']));
+// remove empty values 
+$url = array_values(array_filter($url));
+// and set a default
+if (empty($url[0])) {
+    $url[] = 'home';
+}
+
+switch ($url[0]) {
+    case 'sdg':
+        define('SDG_SLUG', htmlspecialchars($url[1]));
+        include(VIEWS_PATH . 'single.php');
+        break;
+    case 'home':
+    default:
+        include(VIEWS_PATH . 'home.php');
 }
 ?>
 </div>
@@ -54,62 +74,5 @@ foreach ($musicSingles as $single) {
     <p>&copy; 2023 De Containers</p>
     <p>Contact: 35050@ma-web.nl</p>
 </footer>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BO NVVN</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/704f604c3d.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <section>
-        <nav class="nav">
-        <a href=""><img src="img/sdg.svg" alt=""></a> 
-            <ul class="list">
-                <li class="listitem">Over NVVN</li>
-            </ul>
-            <img src="img/SDG-logo-with-UN-Emblem_Horizontal-Web_transparent.png" alt="Description of the image" class="my-image">
-        </a>
-        </nav>
-            <div class="bg">
-            <div class="alletekst">
-                <h1 class="heading">  <img src="" class="logo-image" > Welkom bij SDG Game!</h1>
-                <h2 class="heading2">De SDG-game is een online spel over de 17 duurzame ontwikkelingsdoelen. Dit online spel daagt studenten uit zich te verdiepen in de SDG’s en om challenges uit te voeren zodat zij kennis maken met alle SDG’s volgens de Nederlandse maatstaven.</h2>
-                <a href="http://sdggame.nl/"><button class="grotebutton">Speel nu!</button></a>
-            </div>
-        </div>
-    </section>
-    <section class="onzesdgs">
-        <div class="sdgtekst">SDG's</div>
-        <div class="container">
-            <!-- 5 images above -->
-            <a href="sdg/sdgs.html"><img class="block" src="/img/no-image.png" alt="Image 1"></a>
-            <a href="sdg/sdgs.html"><img class="block" src="/img/no-image.png" alt="Image 2"></a>
-            <a href="sdg/sdgs.html"><img class="block" src="/img/no-image.png" alt="Image 3"></a>
-            <a href="sdg/sdgs.html"><img class="block" src="/img/no-image.png" alt="Image 4"></a>
-            <a href="sdg/sdgs.html"><img class="block" src="/img/no-image.png" alt="Image 5"></a>
-            
-            <!-- 5 images below -->
-            <a href="sdg/sdgs.html"><img class="block" src="/img/no-image.png" alt="Image 6"></a>
-            <a href="sdg/sdgs.html"><img class="block" src="/img/no-image.png" alt="Image 7"></a>
-            <a href="sdg/sdgs.html"><img class="block" src="/img/no-image.png" alt="Image 8"></a>
-            <a href="sdg/sdgs.html"><img class="block" src="/img/no-image.png" alt="Image 9"></a>
-            <a href="sdg/sdgs.html"><img class="block" src="/img/no-image.png" alt="Image 10"></a>
-          </div>
-    </section>
 
 
-</body>
-<footer>
-    <img src="img/red-shipping-container-box-vector-45294776-removebg-preview.png" alt="Image 1">
-    <p>&copy; 2023 De Containers</p>
-    <p>Contact: 35050@ma-web.nl</p>
-</footer>
->>>>>>> 85a87a67ebd292c0a43c3339b71a0162ef9bd6e3:bo/index.html
-</html>
